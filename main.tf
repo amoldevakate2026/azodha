62e3513d1d404e3edbcd2ee1537b7ca0dde38ebb
amoldevakate2026 <amoldevakate2026@gmail.com>
2026-01-18 21:22:07 +0530

    Add IAM instance profile for EKS node group
    This fixes the node registration issue by explicitly creating an IAM instance profile and adding it to the node group dependencies. The EC2 instances were launching but couldn't join the cluster because they had no IAM role attached.

--- main.tf
+++ main.tf
@@ -82,6 +82,12 @@ resource "aws_iam_role_policy_attachment" "azodha-project-AmazonEC2ContainerRegi
   role       = aws_iam_role.azodha-project-1.name
 }
 
+# Create IAM instance profile for node group
+resource "aws_iam_instance_profile" "eks_node_instance_profile" {
+  name = "eks-node-group-instance-profile"
+  role = aws_iam_role.azodha-project-1.name
+}
+
 #create node group
 resource "aws_eks_node_group" "azodha-project" {
   cluster_name    = aws_eks_cluster.azodha-project.name
@@ -102,5 +108,6 @@ resource "aws_eks_node_group" "azodha-project" {
     aws_iam_role_policy_attachment.azodha-project-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.azodha-project-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.azodha-project-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_instance_profile.eks_node_instance_profile,
   ]
 }
